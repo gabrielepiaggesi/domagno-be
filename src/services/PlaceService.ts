3cc4383ac67b43a7ddfe17109cb620376e250611
Gabriele Piaggesi <gabrielepiaggesi@MacBook-Pro-di-Gabriele.local>
2023-06-11 10:02:49 +0200

    port

--- src/services/PlaceService.ts
+++ src/services/PlaceService.ts
@@ -12,7 +12,7 @@ export class PlaceService implements PlaceApi {
         const zl = Math.log2(38000 * Math.cos ( 41.861597 * Math.PI / 180) / radius) + 3;
         console.log('zoom level', zl);
         console.log('km radius', radius);
-        return await Axios.get(`https://serpapi.com/search.json?engine=google_maps&q=${encodeURI(queryString)}&ll=@${lat},${long},${zl.toFixed(1)}z&type=search&api_key=ff9b7f25b03611bd201574335b3ae0334890097cf4ae1b5c9a43e60bbb7f0309`);
+        return await Axios.get(`https://serpapi.com/search.json?engine=google_maps&q=${encodeURI(queryString)}&ll=@${lat},${long},${21}z&type=search&api_key=ff9b7f25b03611bd201574335b3ae0334890097cf4ae1b5c9a43e60bbb7f0309`);
     }
 
 }
